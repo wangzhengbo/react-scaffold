8773c5fda83614c3c86ba1382ddbd5d00891903d
wangzhengbo2 <wangzhengbo2@jd.com>
2020-05-20 16:26:55 +0800

    xxx

--- src/pubicPath.tsx
+++ src/pubicPath.tsx
@@ -1,2 +1,6 @@
 // @ts-ignore
-__webpack_public_path__ = window.static_url
+__webpack_public_path__ = 'http://abc.com/'
+// __webpack_public_path__ = window.static_url
+
+// @ts-ignore
+console.log('__webpack_public_path__', __webpack_public_path__)
